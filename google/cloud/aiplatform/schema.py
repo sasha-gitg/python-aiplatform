9ba041329c79ff514f44641c8807fa272b8625fa
Sasha Sobran <asobran@google.com>
2020-10-28 17:09:25 -0400

    checkpoint

---
+++ google/cloud/aiplatform/schema.py
@@ -0,0 +1,7 @@
+CUSTOM_TASK_GCS_PATH="gs://google-cloud-aiplatform/schema/trainingjob/definition/custom_task_1.0.0.yaml"
+CUSTOM_MODEL_PREDICT_GCS_PATH="gs://google-cloud-aiplatform/schema/trainingjob/definition/custom_task_1.0.0.yaml"
+
+
+class training_job:
+	class definition:
+		custom_task = "gs://google-cloud-aiplatform/schema/trainingjob/definition/custom_task_1.0.0.yaml"
--- google/cloud/aiplatform/training_jobs.py
+++ google/cloud/aiplatform/training_jobs.py
@@ -28,9 +28,28 @@ from typing import Callable, Dict, Optional, Sequence
 from google.auth import credentials as auth_credentials
 from google.cloud.aiplatform import base
 from google.cloud.aiplatform import datasets
+from google.cloud.aiplatform import initializer
 from google.cloud.aiplatform import models
+from google.cloud.aiplatform import schema
+from google.cloud.aiplatform_v1beta1 import AcceleratorType
+from google.cloud.aiplatform_v1beta1 import CustomJobSpec
+from google.cloud.aiplatform_v1beta1 import FractionSplit
+from google.cloud.aiplatform_v1beta1 import GcsDestination
+from google.cloud.aiplatform_v1beta1 import Model
+from google.cloud.aiplatform_v1beta1 import ModelContainerSpec
 from google.cloud.aiplatform_v1beta1 import PipelineServiceClient
+from google.cloud.aiplatform_v1beta1 import PythonPackageSpec
+from google.cloud.aiplatform_v1beta1 import MachineSpec
+from google.cloud.aiplatform_v1beta1 import WorkerPoolSpec
 from google.cloud import storage
+
+
+def _timestamped_gcs_path(root_gcs_path: str, dir_name_prefix: str):
+    timestamp = datetime.datetime.now().isoformat(sep="-", timespec="milliseconds")
+    dir_name = "-".join([dir_name_prefix, timestamp])
+    if root_gcs_path.endswith('/'):
+        root_gcs_path = root_gcs_path[:-1]
+    return '/'.join([root_gcs_path, dir_name])
 
 
 def _timestamped_copy_to_gcs(
@@ -287,39 +306,214 @@ setup(
         return f"{self._ROOT_MODULE}.{self._TASK_MODULE_NAME}"
 
 
-class TrainingJob(base.AiPlatformResourceNoun):
+class CustomTrainingJob(base.AiPlatformResourceNoun):
 
     client_class = PipelineServiceClient
     _is_client_prediction_client = False
 
+    # TODO() add remainder of model optional arguments
     def __init__(
         self,
         display_name: str,
         script_path: str,
         container_uri: str,
+        requirements: Optional[Sequence[str]] = None,
         project: Optional[str] = None,
         location: Optional[str] = None,
         credentials: Optional[auth_credentials.Credentials] = None,
         staging_bucket: Optional[str] = None,
+        model_serving_container_image_uri: Optional[str] = None,
+        model_serving_container_predict_route: Optional[str] = None,
+        model_serving_container_health_route: Optional[str] = None,
     ):
-        pass
+        """Constructs a Custom Training Job.
 
+        Args:
+            project (str):
+                Optional project to retrieve model from. If not set, project set in
+                aiplatform.init will be used.
+            location (str):
+                Optional location to retrieve model from. If not set, location set in
+                aiplatform.init will be used.
+            credentials (auth_credentials.Credentials):
+                Optional credentials to use to retrieve the model.
+        """
+        super().__init__(project=project, location=location, credentials=credentials)
+        self._display_name = display_name
+        self._script_path = script_path
+        self._container_uri = container_uri
+        self._requirements = requirements
+        self._staging_bucket = staging_bucket
+        self._project = project
+        self._credentials = credentials
+        self._model_serving_container_image_uri = model_serving_container_image_uri,
+        self._model_serving_container_predict_route = model_serving_container_predict_route
+        self._model_serving_container_health_route = model_serving_container_health_route
+
+
+
+    # TODO() add filter split, training_pipline.FilterSplit
+    # TODO() add predefined filter split, training_pipeline.PredfinedFilterSplit
+    # TODO() add timestamp split, training_pipeline.TimestampSplit
+    # TODO() add scheduling, custom_job.Scheduling
     def run(
         self,
         dataset: Optional[datasets.Dataset],
-        output_dir: Optional[str] = None,
+        base_output_dir: Optional[str] = None,
         args: Optional[Dict[str, str]] = None,
-        replica_count=0,
-        machine_type="n1-standard-4",
-        accelerator_type=None,
-        accelerator_count=0,
-        spec_yaml_path: Optional[str] = None,
+        replica_count: str=0,
+        machine_type: str="n1-standard-4",
+        accelerator_type: str='ACCELERATOR_TYPE_UNSPECIFIED',
+        accelerator_count: int=0,
+        model_display_name: Optional[str] = None,
+
+        training_fraction_split: float = 1.0,
+        validation_fraction_split: float = 0.0,
+        test_fraction_split: float = 0.0,
+
+
     ) -> models.Model:
-        pass
+        """Runs the custom training job.
+
+        Any of ``training_fraction_split``, ``validation_fraction_split`` and
+        ``test_fraction_split`` may optionally be provided, they must sum to up to 1. If
+        the provided ones sum to less than 1, the remainder is assigned to sets as
+        decided by AI Platform.If none of the fractions are set, by default roughly 80%
+        of data will be used for training, 10% for validation, and 10% for test.
+
+        Args:
+
+            base_output_dir (str):
+                GCS output directory of job. If not provided a 
+                timestamped directory in the staging directory will be used.
+            accelerator_type (str):
+                Hardware accelerator type. One of ACCELERATOR_TYPE_UNSPECIFIED,
+                NVIDIA_TESLA_K80, NVIDIA_TESLA_P100, NVIDIA_TESLA_V100, NVIDIA_TESLA_P4,
+                NVIDIA_TESLA_T4, TPU_V2, TPU_V3
+            training_fraction_split (float):
+                The fraction of the input data that is to be
+                used to train the Model.
+            validation_fraction_split (float):
+                The fraction of the input data that is to be
+                used to validate the Model.
+            test_fraction_split (float):
+                The fraction of the input data that is to be
+                used to evaluate the Model.
+
+        Returns:
+            The trainer model resource.
+        """
+
+        def flatten_args(args):
+            return [f"--{key}={value}" for key, value in args.items()]
 
 
-class CustomTrainingJob(TrainingJob):
-    pass
+        # TODO: add logging about uploading python package
+        # Make python package
+        python_packager = TrainingScriptPythonPackager(
+            script_path=self._script_path,
+            requirements=self._requirements
+        )
+
+        package_gcs_uri = python_packager.package_and_copy_to_gcs(
+            gcs_staging_dir = self._staging_bucket,
+            project = self._project or initializer.global_config.project,
+            credentials = self._credentials or initializer.global_config.credentials,
+        )
+
+        # Create Package spec
+        python_package_spec = PythonPackageSpec(
+            executor_image_uri=self._container_uri,
+            package_uris=[package_gcs_uri],
+            python_model=python_packager.module_name,
+            args=flatten_args(args)
+        )
+
+        # Create machine spec
+        machine_spec = MachineSpec(
+            machine_type=machine_type,
+            accelerator_type=getattr(AcceleratorType, accelerator_type,
+                AcceleratorType.ACCELERATOR_TYPE_UNSPECIFIED),
+            accelerator_count=accelerator_count
+
+        )
+
+        # Create worker pool spec
+        worker_pool_spec = WorkerPoolSpec(
+            python_package_spec=python_package_spec,
+            machine_spec=machine_spec,
+            replica_count=replica_count
+        )
+
+        # Create fraction split spec
+        fraction_split = FractionSplit(
+            training_fraction=training_fraction,
+            validation_fraction=validation_fraction,
+            test_fraction=test_fraction
+        )
+
+        # set base output directory for training
+        base_output_dir = base_output_dir or _get_timestamped_gcs_dir(
+            self._staging_bucket or initializer.global_config.staging_bucket,
+            'aiplatform-custom-training')
+
+        # create custom job spec
+        custom_job_spec = CustomJobSpec(
+            worker_pool_spec=worker_pool_spec,
+            gcs_destination=GcsDestination(output_uri_prefix=base_output_dir)
+
+        )
+
+        
+        managed_model = None
+        # Validate if model should be uploaded and create model payload
+        if model_display_name:
+
+            # if args need for model is incomplete
+            # TODO (b/162273530) lift requirement for predict/health route when
+            # validation lifted and move these args down
+            if not all(self._model_serving_container_image_uri,
+            self._model_serving_container_predict_route,
+            self._model_serving_container_health_route):
+                
+                raise RuntimeError("""model_display_name was provided but 
+                    model_serving_container_image_uri,
+                    model_serving_container_predict_route, and
+                    model_serving_container_health_route were not provided when this 
+                    custom job was constructed.
+                    """)
+
+
+            container_spec = ModelContainerSpec(
+                image_uri=self._model_serving_container_image_uri,
+                predict_route=self._serving_container_predict_route,
+                health_route=self._serving_container_health_route,
+            )
+
+            managed_model = Model(
+                display_name=model_display_name,
+                container_spec=container_spec
+            )
+
+        # create input data config
+        
+            
+            
+        # create training pipeline
+        training_pipeline = TrainingPipeline(
+            display_name = self._display_name,
+            training_task_definition = schema.training_job.definition.custom_task,
+            training_task_inputs= custom_job_spec,
+            model_to_upload=managed_model
+        )
+
+
+
+
+
+
+
+
 
 
 class AutoMLTablesTrainingJob(TrainingJob):
