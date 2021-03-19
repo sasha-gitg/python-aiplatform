d6ea7e29b09c372f425320c8f0364c9c5f3bd1a3
Sasha Sobran <asobran@google.com>
2021-03-19 11:10:52 -0400

    add new ops and reduce polling wait

--- google/cloud/aiplatform/pipelines/__init__.py
+++ google/cloud/aiplatform/pipelines/__init__.py
@@ -3,7 +3,11 @@ from google.cloud.aiplatform.pipelines import utils
 
 DatasetCreateOp = utils.convert_method_to_component(aiplatform.Dataset.create)
 
+DatasetExportDataOp = utils.convert_method_to_component(aiplatform.Dataset.export_data, should_serialize_init=True)
+
 CustomContainerTrainingJobRunOp = utils.convert_method_to_component(aiplatform.CustomContainerTrainingJob.run,
                                                   should_serialize_init=True)
 
 ModelDeployOp = utils.convert_method_to_component(aiplatform.Model.deploy, should_serialize_init=True)
+
+ModelUploadOp = utils.convert_method_to_component(aiplatform.Model.upload, should_serialize_init=True)
